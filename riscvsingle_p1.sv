d92e0dcd8c5eed62930a97f82b136a38276d09bd
PedroN3ves <pedroholiveira784@gmail.com>
2026-04-19 22:57:37 -0300

    Mapeando os 11 bits da instrucao addi no module maindec

--- riscvsingle_p1.sv
+++ riscvsingle_p1.sv
@@ -130,6 +130,7 @@ module maindec(input  logic [6:0] op,
       7'b0100011: controls = 11'b0_01_1_1_00_0_00_0; // sw
       7'b0110011: controls = 11'b1_xx_0_0_00_0_10_0; // R-type 
       7'b1100011: controls = 11'b0_10_0_0_00_1_01_0; // beq
+      7'b0010011: controls = 11'b1_00_1_0_00_0_10_0; //* addi (estava faltando, adicionado agora)
       default:    controls = 11'bx_xx_x_x_xx_x_xx_x; // non-implemented instruction
     endcase
 endmodule
